7c2f659d6d4876ace691daf5f892438417a15695
handaoui-mohamed <handaoui.mohamed@gmail.com>
2017-09-28 12:52:48 +0100

    updated front to final version

--- config/config.js
+++ config/config.js
@@ -5,42 +5,42 @@ require('dotenv').config();
 
 // define validation for all the env vars
 const envVarsSchema = Joi.object({
-  NODE_ENV: Joi.string()
-    .allow(['development', 'production', 'test', 'provision'])
-    .default('development'),
-  PORT: Joi.number()
-    .default(4040),
-  MONGOOSE_DEBUG: Joi.boolean()
-    .when('NODE_ENV', {
-      is: Joi.string().equal('development'),
-      then: Joi.boolean().default(true),
-      otherwise: Joi.boolean().default(false)
-    }),
-  JWT_SECRET: Joi.string()
-    .default('ksdhfskdhfiuzefzef64sd5f4sfe@#à@')
-    .description('JWT Secret required to sign'),
-  MONGO_HOST: Joi.string()
-    .default('localhost')
-    .description('Mongo DB host url'),
-  MONGO_PORT: Joi.number()
-    .default(27017)
+	NODE_ENV: Joi.string()
+		.allow(['development', 'production', 'test', 'provision'])
+		.default('development'),
+	PORT: Joi.number()
+		.default(4040),
+	MONGOOSE_DEBUG: Joi.boolean()
+		.when('NODE_ENV', {
+			is: Joi.string().equal('development'),
+			then: Joi.boolean().default(true),
+			otherwise: Joi.boolean().default(false)
+		}),
+	JWT_SECRET: Joi.string()
+		.default('ksdhfskdhfiuzefzef64sd5f4sfe@#à@')
+		.description('JWT Secret required to sign'),
+	MONGO_HOST: Joi.string()
+		.default('localhost/gvol-database')
+		.description('Mongo DB host url'),
+	MONGO_PORT: Joi.number()
+		.default(27017)
 }).unknown()
-  .required();
+	.required();
 
 const { error, value: envVars } = Joi.validate(process.env, envVarsSchema);
 if (error) {
-  throw new Error(`Config validation error: ${error.message}`);
+	throw new Error(`Config validation error: ${error.message}`);
 }
 
 const config = {
-  env: envVars.NODE_ENV,
-  port: envVars.PORT,
-  mongooseDebug: envVars.MONGOOSE_DEBUG,
-  jwtSecret: envVars.JWT_SECRET,
-  mongo: {
-    host: envVars.MONGO_HOST,
-    port: envVars.MONGO_PORT
-  }
+	env: envVars.NODE_ENV,
+	port: envVars.PORT,
+	mongooseDebug: envVars.MONGOOSE_DEBUG,
+	jwtSecret: envVars.JWT_SECRET,
+	mongo: {
+		host: envVars.MONGO_HOST,
+		port: envVars.MONGO_PORT
+	}
 };
 
 export default config;
